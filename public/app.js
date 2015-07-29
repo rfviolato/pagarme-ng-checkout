26b523ab284e41840077e2bba4525a35d365ef2d
Rafael Violato <rfviolato@gmail.com>
2015-07-29 10:25:59 -0300

    Create checkout load promise

--- public/app.js
+++ public/app.js
@@ -2,11 +2,30 @@
 (function(){
 
 	angular.module('pg-ng-checkout-demo', ['pg-ng-checkout'])
-	.config(config);
+	.config(config)
+	.run(run);
 
 	function config($pgCheckoutProvider){
 
+		$pgCheckoutProvider.setEncryptKey('ek_test_mSVAcBZidrQieIX53fWPlLfchMMk6A');
 		
+	}
+
+	function run($pgCheckout){
+
+		var checkoutPromise = $pgCheckout.getCheckout();
+
+		checkoutPromise.then(function(checkout){
+
+			console.log(checkout);
+			
+		});
+
+		checkoutPromise.then(function(checkout){
+
+			console.log(checkout);
+			
+		});
 		
 	}
 	
--- src/js/pg-ng-checkout.js
+++ src/js/pg-ng-checkout.js
@@ -6,25 +6,74 @@
 
 	function PgCheckout(){
 
-		var apiKey;
+		var _encryptKey;
 
 		return{
+
+			//config methods
+			setEncryptKey: setEncryptKey,
 
 			//injectable service
 			$get: $get,
 
-			//config methods
-			setApiKey: setApiKey,
-
 		}
 
-		function setApiKey(key){
+		function setEncryptKey(key){
 
-			apiKey = key;
+			_encryptKey = key;
 			
 		}
 
-		function $get(){
+		function $get($q, $interval){
+
+			var checkout;
+			var get = {
+
+				getCheckout: getCheckout,
+
+			};
+
+			return get;
+
+			function getCheckout(){
+
+
+				var promise = $q(function(resolve, reject){
+
+					var elapsedTime = 0;
+					var intervalTime = 70;
+					var interval = $interval(function(){
+
+						if(PagarMeCheckout){
+
+							if(PagarMeCheckout.Checkout && _encryptKey){
+
+								checkout = new PagarMeCheckout.Checkout({'encryption_key': _encryptKey});
+
+								$interval.cancel(interval);
+
+								resolve(checkout);
+
+							}
+
+						}else if(elapsedTime === 10000){
+
+							$interval.cancel(interval);
+							reject('load timeout');
+
+						}
+
+						elapsedTime += intervalTime;
+						
+					},intervalTime);
+
+					
+				});
+
+				return promise;
+
+				
+			}
 			
 		}
 		
